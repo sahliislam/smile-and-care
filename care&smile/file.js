ab1cf5d8db713e292dc6d157eebf4ba5a6d29629
sahliislam <48824194+sahliislam@users.noreply.github.com>
2019-07-28 16:35:00 +0200

    home 90%

---
+++ care&smile/file.js
@@ -0,0 +1,6 @@
+M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.slider');
+      var instances = M.Slider.init(elems, {});
+    }); 
+ $(".dropdown-trigger").dropdown();
