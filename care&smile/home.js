032b768bee000aae911c9fae03a47ac74597b472
sahliislam <48824194+sahliislam@users.noreply.github.com>
2019-07-28 02:24:22 +0200

    home first step

---
+++ care&smile/home.js
@@ -0,0 +1,6 @@
+<script>M.AutoInit();</script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);});
+</script>
