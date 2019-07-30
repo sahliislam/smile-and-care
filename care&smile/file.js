9ed979df90e6d6f9ff6e2ce77c2900bb47783042
sahliislam <48824194+sahliislam@users.noreply.github.com>
2019-07-30 17:57:42 +0200

    70% project

--- care&smile/file.js
+++ care&smile/file.js
@@ -4,3 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
       var instances = M.Slider.init(elems, {});
     }); 
  $(".dropdown-trigger").dropdown();
+
+ 
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tap-target');
+  var instances = M.TapTarget.init(elems, options);
+});
+
